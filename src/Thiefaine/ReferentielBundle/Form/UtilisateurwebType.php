ea849d90c89a56f7938fdb99c6414800975ba1f7
Theraud Mickael <theraud.mickael@hotmail.fr>
2014-05-09 16:52:01 +0200

    utilisateur > done 80% > need change button

--- src/Thiefaine/ReferentielBundle/Form/UtilisateurwebType.php
+++ src/Thiefaine/ReferentielBundle/Form/UtilisateurwebType.php
@@ -15,14 +15,85 @@ class UtilisateurwebType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('login')
-            ->add('password')
-            ->add('email')
-            ->add('infos')
-            ->add('idgroupe')
-        ;
+            ->add('nom', 'text', array(
+                'label' => 'Nom',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Nom',
+                    'title' => 'Renseigner le nom de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('prenom', 'text', array(
+                'label' => 'Prenom',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Prenom',
+                    'title' => 'Renseigner le prenom de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('login', 'text', array(
+                'label' => 'Login',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Login',
+                    'title' => 'Renseigner le login de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('password', 'text', array(
+                'label' => 'Password',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Password',
+                    'title' => 'Renseigner le password de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('email', 'email', array(
+                'label' => 'Email',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Email',
+                    'title' => 'Renseigner le email de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('infos', 'textarea', array(
+                'label' => 'Informations',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder' => 'Informations',
+                    'title' => 'Renseigner les informations de l\'utilisateur.',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'right'
+                )
+            ))
+            ->add('idgroupe');
     }
     
     /**
