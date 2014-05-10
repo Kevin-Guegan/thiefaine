2fb19d09ff066cb8656999a25bd2792f2fe66bff
Christopher <christopher.louet@gmail.com>
2014-05-10 22:07:38 +0200

    generate entities

---
+++ src/Thiefaine/ReferentielBundle/Entity/Alerte.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Thiefaine\ReferentielBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Alerte
+ */
+class Alerte
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Thiefaine\ReferentielBundle\Entity\Message
+     */
+    private $idmessage;
+
+    /**
+     * @var \Thiefaine\ReferentielBundle\Entity\Zone
+     */
+    private $idzone;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idmessage
+     *
+     * @param \Thiefaine\ReferentielBundle\Entity\Message $idmessage
+     * @return Alerte
+     */
+    public function setIdmessage(\Thiefaine\ReferentielBundle\Entity\Message $idmessage = null)
+    {
+        $this->idmessage = $idmessage;
+
+        return $this;
+    }
+
+    /**
+     * Get idmessage
+     *
+     * @return \Thiefaine\ReferentielBundle\Entity\Message 
+     */
+    public function getIdmessage()
+    {
+        return $this->idmessage;
+    }
+
+    /**
+     * Set idzone
+     *
+     * @param \Thiefaine\ReferentielBundle\Entity\Zone $idzone
+     * @return Alerte
+     */
+    public function setIdzone(\Thiefaine\ReferentielBundle\Entity\Zone $idzone = null)
+    {
+        $this->idzone = $idzone;
+
+        return $this;
+    }
+
+    /**
+     * Get idzone
+     *
+     * @return \Thiefaine\ReferentielBundle\Entity\Zone 
+     */
+    public function getIdzone()
+    {
+        return $this->idzone;
+    }
+}
