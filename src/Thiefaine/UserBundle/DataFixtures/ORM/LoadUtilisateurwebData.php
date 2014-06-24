f80f8c254d8e6246d0228404622fc9e81c46f408
root <root@debian>
2014-06-24 17:11:09 +0200

    Ajout fixtures adminuser

---
+++ src/Thiefaine/UserBundle/DataFixtures/ORM/LoadUtilisateurwebData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Thiefaine\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Thiefaine\UserBundle\Entity\Utilisateurweb;
+
+class LoadUtilisateurwebData extends AbstractFixture implements OrderedFixtureInterface
+{
+	public function load(ObjectManager $em)
+	{
+		$admin = new Utilisateurweb();
+		$admin->setUsername('adminuser');
+		$admin->setEmail('admin@admin.user');
+        $admin->setPlainPassword('admin');
+        $admin->setEnabled(true);
+
+		$em->persist($admin);
+		$em->flush();
+
+		$this->addReference('utilisateurweb-admin', $admin);
+	}
+
+	public function getOrder()
+	{
+		return 1;
+	}
+}
