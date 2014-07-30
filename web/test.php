e54a1ddc3608359f22e5af48e685f504f94a7618
christopherlouet <christopher.louet@gmail.com>
2014-07-30 21:57:21 +0200

    suppression de test.php

--- web/test.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-$test = json_decode('{"id":1,"titre":"test","message":"<p>christopher lou&euml;t<\/p>","datecreation":"2014-07-29T21:44:10+0200","datemiseajour":"2014-07-30T21:03:25+0200","utilisateurweb":{"id":1,"username":"adminuser","username_canonical":"adminuser","email":"admin@admin.user","email_canonical":"admin@admin.user","enabled":true,"salt":"mihc2e4qwhcsoskck08w0wgggkkc0ks","password":"CBEhfKv3A8PCBxbPEBjE+awPAYx\/txjHsaAUDM79do2aXWrOjZkiXMLcEQi6o4wFyMLdTZDiGe+AHPfunWXuOA==","last_login":"2014-07-30T21:01:20+0200","groups":[],"locked":false,"expired":false,"roles":["ROLE_SUPER_ADMIN"],"credentials_expired":false,"nom":"admin","prenom":"admin","conseils":{"1":{"id":2,"titre":"cdls,jclcskdcl,kcs,k","message":"<p>scqsqqcsqcs<\/p>","datecreation":"2014-07-30T21:01:32+0200","categories":[]}},"informations":[{"id":1,"titre":"cacacaca","message":"<p>xsxsxsxsxs<\/p>","datecreation":"2014-07-29T21:43:59+0200","alerte":false,"zone":{"id":1,"nom":"toto","points":[]},"categories":[{"id":3,"libelle":"pupute"}]},{"id":10,"titre":"pipipipipi","message":"<p>pipipippi<\/p>","datecreation":"2014-07-29T21:43:59+0200","alerte":false,"zone":{"id":1,"nom":"toto","points":[]},"categories":[{"id":3,"libelle":"pupute"}]}],"zones":[{"id":1,"nom":"toto","points":[]}]},"categories":[]}');
-
-return $test;
-
-
-?>
