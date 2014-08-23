7974b3aa3d9b64250a835921ac0e6b06d721cd2c
christopherlouet <christopher.louet@gmail.com>
2014-08-23 18:32:16 +0200

    Mise à jour suppression utilisateur

--- src/Thiefaine/ReferentielBundle/Controller/UtilisateurwebController.php
+++ src/Thiefaine/ReferentielBundle/Controller/UtilisateurwebController.php
@@ -108,6 +108,21 @@ class UtilisateurwebController extends Controller
             return $this->redirect($this->generateUrl('thiefaine_referentiel_utilisateurweb_list'));
         }
 
+        // on met à jour les conseils, infos et zones de l'utilsateur => on met à null l'id utilisateur
+        $conseils = $user->getConseils();
+        $informations = $user->getInformations();
+        $zones = $user->getZones();
+
+        foreach ($conseils as $conseil) {
+            $conseil->setUtilisateurweb(null);
+        }
+        foreach ($informations as $information) {
+            $information->setUtilisateurweb(null);
+        }
+        foreach ($zones as $zone) {
+            $zone->setUtilisateurweb(null);
+        }
+
         $em->remove($user);
         $em->flush();
 
